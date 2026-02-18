7a696becb4ed172b7f31383bf943085e4412bf6a
cwmiller192-collab <cwmiller192@gmail.com>
2026-02-18 10:42:38 -0500

    Restored app/page.tsx file and removed the yellow background section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,13 @@
-<PUT COMPLETE CONTENT OF THE FILE WITHOUT YELLOW BACKGROUND SECTION HERE>
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Welcome to the Evergreen site!</h1>
+      {/* Other components */}
+      {/* Removed yellow background section */}
+    </div>
+  );
+};
+
+export default Page;
